c48c062594f656472e53485297bb33c4a13b69db
RoanKathrina <52074737+RoanKathrina@users.noreply.github.com>
2024-01-18 22:40:02 -0500

    Delete Assignment 01 - Solution directory

--- Assignment 01 - Solution/Roan_Kathrina_Dimaculangan.py
+++
@@ -1,4 +0,0 @@
-def hi_my_name_is():
-    return "Roan Kathrina Dimaculangan" # Write your name inside the quotation marks
-
-print(hi_my_name_is())
--- Assignment 01 - Solution/ex1_test.py
+++
@@ -1,4 +0,0 @@
-import Roan_Kathrina_Dimaculangan as name_point
-
-def test_hi_my_name_is():
-    assert len(name_point.hi_my_name_is())  > 1 
